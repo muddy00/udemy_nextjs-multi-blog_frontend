84fcdc1e379afe095fc9859f92a96c7ca0c4ca2e
tsuzuki <tsuzuki@tsuzukinoiMac.local>
2022-01-17 18:33:40 +0900

    end of lesson42. admin and user dashboard

--- components/auth/SigninComponent.jsx
+++ components/auth/SigninComponent.jsx
@@ -31,7 +31,11 @@ const SigninComponent = () => {
         // save user info to localStorage
         // authenticate user
         authenticate(data, () => {
-          Router.push("/")
+          if (isAuth() && isAuth().role === 1) {
+            Router.push("/admin")
+          } else {
+            Router.push("/user")
+          }
         })
       }
     })
---
+++ pages/user/index.js
@@ -0,0 +1,11 @@
+import Layout from "../../components/Layout"
+
+const UserIndex = () => {
+  return (
+    <Layout>
+      <h2>User Dashborad</h2>
+    </Layout>
+  )
+}
+
+export default UserIndex
